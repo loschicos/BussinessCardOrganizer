12d35ade15dc3eb6eaf76a4e86e2e8d97313b682
loschicos <loschicossoftware@gmail.com>
2015-06-25 20:58:12 -0300

    First Commit

---
+++ app/src/main/java/com/loschicos/bussinesscardorganizer/activities/MainActivity.java
@@ -0,0 +1,39 @@
+package com.loschicos.bussinesscardorganizer.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.loschicos.bussinesscardorganizer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.main_action_about: {
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
