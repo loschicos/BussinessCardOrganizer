52978e6de09d00b1f89529552784f7cbe315a16e
loschicos <loschicossoftware@gmail.com>
2015-06-25 20:55:15 -0300

    Teste

---
+++ app/src/main/java/com/loschicos/bussinesscardorganizer/activities/AboutActivity.java
@@ -0,0 +1,32 @@
+package com.loschicos.bussinesscardorganizer.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.loschicos.bussinesscardorganizer.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home: {
+                finish();
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
